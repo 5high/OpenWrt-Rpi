f095163ad9d8d3fdd5587f447aa38038eac52264
5high <5high.net@gmail.com>
2022-05-30 21:03:56 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -12,6 +12,14 @@ uci set luci.main.mediaurlbase='/luci-static/argon'
 # Disable IPV6 ula prefix
 sed -i 's/^[^#].*option ula/#&/' /etc/config/network
 
+# config udpxy
+uci set udpxy.@udpxy[0].mcsub_renew='300'
+uci set udpxy.@udpxy[0].buffer_size='2Mb'
+uci set udpxy.@udpxy[0].bind='0.0.0.0'
+uci set udpxy.@udpxy[0].source='0.0.0.0'
+uci set udpxy.@udpxy[0].disabled='0'
+
+
 # Check file system during boot
 uci set fstab.@global[0].check_fs=1
 uci commit
